a3dfa5400c27b62968a5bde9cb8081a632e81386
Tom Vaillant <tom.vaillant.vt@gmail.com>
2025-10-28 14:58:11 +0100

    ajout des migrations

---
+++ back/database/migrations/2025_10_28_134956_horaire_migration.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('horaires', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('entree_matin');
+            $table->timestamp('sortie_midi');
+            $table->timestamp('entree_midi');
+            $table->timestamp('sortie_soir');
+            $table->foreignId('utilisateur_id')->constrained('utilisateurs')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('horaires');
+    }
+};
